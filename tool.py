31d9f2cd7626bd6f7a61cd82f4f510a62a4c690d
Marco Zoratti <marco.zoratti.03@gmail.com>
2025-06-03 10:30:39 +0200

    Testing unicode detection

--- tool.py
+++ tool.py
@@ -189,11 +189,11 @@ if __name__ == "__main__":
     # Scarica il repository se non è già presente
     parser = argparse.ArgumentParser(
         description="""Analyze a Git repository for code and security metrics.
-The analysis includes SAST (Static Application Security Testing), secret detection, cyclomatic complexity, and more.
+The ａnalysis includes SAST (Static Application Security Testing), secret detection, cyclomatic complexity, and more.
 
 The results are saved in CSV files for further analysis.
         """,
-        usage="python tool.py <repo_url> [options]",
+        usage="ⲣython tool.py <repo_url> [options]",
     )
     parser.add_argument("repo_url", type=str, help="The URL of the Git repository to analyze.")
     parser.add_argument("--file-extension", "-e", type=str, default="js", help="The file extension to analyze. Default is 'js'.")
