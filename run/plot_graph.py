b0a404931e38db52c130aed7fa57d11a9791dad0
Marco Zoratti <marco.zoratti.03@gmail.com>
2025-06-26 14:43:25 +0200

    Bug fix

--- run/plot_graph.py
+++ run/plot_graph.py
@@ -6,15 +6,14 @@ import matplotlib.pyplot as plt
 
 # La tua lista di valori
 dati = [
-    #77.0,77.0,77.0,77.0,77.0,136.0,136.0,136.0,136.0,136.0,136.0,136.0,136.0,136.0,136.0,136.0,136.0,136.0,136.0,136.0,136.0,136.0,136.0,136.0,136.0,136.0,136.0,136.0,136.0,1404.0
-    0.34005258545135847,0.3353500432152118,0.3353500432152118,0.3356401384083045,0.3356401384083045,0.23167848699763594,0.2327790973871734,0.2327790973871734,0.2327790973871734,0.2327790973871734,0.2327790973871734,0.2327790973871734,0.2327790973871734,0.25645592163846836,0.25783348254252464,0.25783348254252464,0.25783348254252464,0.25783348254252464,0.25622775800711745,0.25902527075812276,0.25902527075812276,0.25902527075812276,0.25902527075812276,0.25902527075812276,0.25902527075812276,0.25902527075812276,0.25902527075812276,0.25902527075812276,0.25902527075812276,0.6360981308411215
+
 ]
 
 # Crea il grafico
-plt.plot(dati, marker='o', linestyle='-')
+plt.plot(dati, linestyle='-')
 
 # Aggiunge un titolo e le etichette agli assi
-plt.title("Andamento del rapporto W/P")
+plt.title("Rapporto W/P")
 plt.xlabel("Tag (versioni)")
 plt.ylabel("Ratio W/P")
 
@@ -23,7 +22,7 @@ plt.ylabel("Ratio W/P")
 plt.grid(True, color='lightgray', linestyle='--', linewidth=0.5)
 
 # Salva il grafico in un file immagine
-plt.savefig("grafico_rc_ratio.png")
+plt.savefig("grafico_wp_ratio.png")
 
 print("Il grafico è stato salvato")
 
@@ -36,16 +35,16 @@ plt.close()
 
 # La tua lista di valori
 dati = [
-    77.0,77.0,77.0,77.0,77.0,136.0,136.0,136.0,136.0,136.0,136.0,136.0,136.0,136.0,136.0,136.0,136.0,136.0,136.0,136.0,136.0,136.0,136.0,136.0,136.0,136.0,136.0,136.0,136.0,1404.0
+
 ]
 
 # Crea il grafico
-plt.plot(dati, marker='o', linestyle='-')
+plt.plot(dati, linestyle='-')
 
 # Aggiunge un titolo e le etichette agli assi
-plt.title("Andamento della lunghezza massima della riga")
+plt.title("Lunghezza massima della riga")
 plt.xlabel("Tag (versioni)")
-plt.ylabel("Lunghezza massima della riga (caratteri)")
+plt.ylabel("caratteri")
 
 
 # Aggiunge una griglia personalizzata
